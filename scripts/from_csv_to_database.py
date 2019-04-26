86c4af53a4fc3995afbd590416ae243d3b799e66
Bruno B. Scholl <bruno.scholl@usp.br>
2019-04-26 13:55:35 -0300

    Hotfix
    Signed-off-by: Bruno B. Scholl <bruno.scholl@usp.br>

--- scripts/from_csv_to_database.py
+++ scripts/from_csv_to_database.py
@@ -491,7 +491,7 @@ def section_2_7(csv_file, cursor):
                 aux_fields['line_research'][2] = insert_aux(
                     cursor, 'line_research', row[7])
 
-                for col, _ in enumerate(row[2:6]):
+                for col in range(2,6):
                     if row[col] == '':
                         row[col] = None
 
